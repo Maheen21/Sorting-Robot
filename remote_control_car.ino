f17927529502a2161922700c5e043dd71e576074
Maheen Aftab <124914805+Maheen21@users.noreply.github.com>
2024-11-15 00:15:04 -0700

    Update remote_control_car.ino

--- remote_control_car.ino
+++ remote_control_car.ino
@@ -1,7 +1,7 @@
 float x_joystick;
 float y_joystick;
 
-// put your setup code here, to run once:
+// Motor Pins:
 int motor1pin1 = 2;
 int motor1pin2 = 3;
 
@@ -10,7 +10,7 @@ int motor2pin2 = 5;
 
 void setup() {
   Serial.begin(9600);
-  // put your setup code here, to run once:
+  // setup code:
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
   pinMode(motor2pin1,   OUTPUT);
@@ -19,34 +19,35 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // input from controller:
   x_joystick = pulseIn(A0, HIGH); 
   y_joystick = pulseIn(A1, HIGH);
+  // print coordinates
   Serial.print("x:");
   Serial.println(x_joystick);
   Serial.print("y:");
   Serial.println(y_joystick);
+  
   delay(50);
 
-   // put your main code here, to run repeatedly:
-
-  //Controlling speed (0   = off and 255 = max speed):    
-  //(Optional)
-  analogWrite(9, 200); //ENA   pin
-  analogWrite(10, 200); //ENB pin
-  //(Optional)
+  //Controlling speed (0  = off and 255 = max speed):    
+  analogWrite(9, 200); //ENA   pin (sets speed for front motors)
+  analogWrite(10, 200); //ENB pin (sets speed for back motors)
+  // Go forward
  if (x_joystick > 1900 ){
    digitalWrite(motor1pin1,   LOW); // front
    digitalWrite(motor1pin2, HIGH);
    digitalWrite(motor2pin1,   LOW);
    digitalWrite(motor2pin2, HIGH);
  }
+   // Go Backwards
   else if (x_joystick < 100){
    digitalWrite(motor1pin1,   HIGH); //back
    digitalWrite(motor1pin2, LOW);
    digitalWrite(motor2pin1,   HIGH);
    digitalWrite(motor2pin2, LOW);
  }
+  // Turn Right
  else if (y_joystick < 100){
    analogWrite(9, 10); //ENA   pin
    analogWrite(10, 10); //ENB pin
@@ -55,6 +56,7 @@ void loop() {
    digitalWrite(motor2pin1,   HIGH);
    digitalWrite(motor2pin2, LOW);
  }
+  // Turn Left
  else if (y_joystick > 1800){
    analogWrite(9, 10); //ENA   pin
    analogWrite(10, 10); //ENB pin
@@ -63,6 +65,7 @@ void loop() {
    digitalWrite(motor2pin1,   LOW);
    digitalWrite(motor2pin2, HIGH);
  }
+   // Stop if joystick is not being moved
  else if (x_joystick >= 100 || x_joystick < 1900 && y_joystick >= 100 || y_joystick < 1900) {
    digitalWrite(motor1pin1,   LOW);
    digitalWrite(motor1pin2, LOW);
